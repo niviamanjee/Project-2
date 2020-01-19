bad3a34ba80c24c28a1b7356e09c301507bd88e8
Nivi Amanjee <nivedita.amanjee@gmail.com>
2020-01-19 15:56:02 -0800

    able to show card image for each recipe, working on creating a modal that will display recipe information after view recipe button is clicked

--- public/assets/js/spoonacular-api.js
+++ public/assets/js/spoonacular-api.js
@@ -2,15 +2,6 @@ const API_KEY = "f00927d28fd14a4fa274e892d9a2af03";
 
 var ingredientList = [];
 var items = ingredientList.map(ingredients => ingredients.toLowerCase());
-//function that gets ingredients from user input
-// function getIngredients() {
-//   $(".addItem").on("click", function(event) {
-//     event.preventDefault();
-//     var ingredient = ingredientList.push(ingredient);
-//   });
-//   console.log(ingredientList);
-// }
-//functiont that gets recipe ID's of ingredients users inputed
 
 $("#addToList").on("click", function(event) {
   $(".itemBox").empty();
@@ -55,6 +46,8 @@ $("#addToList").on("click", function(event) {
     // document.getElementById("animal-form").reset();
     // createButtons();
   }
+
+  $("#addItem").val("");
 });
 // for (var i = 0; i < ingredientList.length; i++) {
 // }
@@ -69,7 +62,7 @@ function getRecipeIDs(list) {
   var queryUrl =
     "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
     ingredients +
-    "&number=20&instructionsRequired=true&apiKey=" +
+    "&number=5&instructionsRequired=true&apiKey=" +
     API_KEY;
 
   $.ajax({
@@ -77,15 +70,105 @@ function getRecipeIDs(list) {
     method: "GET"
   }).then(
     function(result) {
+      var recipeIdArray = [];
       for (var i = 0; i < result.length; i++) {
         console.log(`result: ${result[i].id}`);
+        recipeIdArray.push(result[i].id);
       }
       // return result
-
-      console.log(`Query URL: ${queryUrl}`);
+      getRecipeInfo(recipeIdArray);
+      // console.log(`Query URL: ${queryUrl}`);
     },
     function(error) {
       console.log(error);
     }
   );
 }
+
+function getRecipeInfo(list) {
+  for (var i = 0; i < list.length; i++) {
+    var queryUrl =
+      "https://api.spoonacular.com/recipes/" +
+      list[i].toString() +
+      "/information?includeNutrition=false&number=5&instructionsRequired=true&apiKey=" +
+      API_KEY;
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).then(
+      function(result) {
+        // var recipeIdArray = [
+
+        // for (var i = 0; i < result.length; i++) {
+        console.log(`Recipe name: ${result.title}`);
+        console.log(result);
+        //   recipeIdArray.push(result[i].id);
+        // }
+        // return result
+        if (result.analyzedInstructions.length > 0) {
+          for (var i = 0; i < result.analyzedInstructions.length; i++) {
+            console.log(result.analyzedInstructions[i].steps);
+            for (j = 0; j < result.analyzedInstructions[i].steps.length; j++) {
+              var stepNumber = result.analyzedInstructions[i].steps[j].number;
+              var stepText = result.analyzedInstructions[i].steps[j].step;
+
+              // console.log(`${stepNumber}. ${stepText}`);
+
+              //<div class="card" style="width: 18rem;">
+              //   <img src="..." class="card-img-top" alt="...">
+              //   <div class="card-body">
+              //     <h5 class="card-title">Card title</h5>
+              //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              //     <a href="#" class="btn btn-primary">Go somewhere</a>
+              //   </div>
+              // </div>
+            }
+          }
+          var cardDiv = $("<div>")
+            .addClass("card")
+            .attr("style", "width: 18rem;");
+          var image = $("<img>")
+            .attr("src", `${result.image}`)
+            .addClass("card-img-top")
+            .attr("alt", `${result.title}`);
+          var contentDiv = $("<div>").addClass("card-body");
+          var cardTitle = $("<h5>")
+            .addClass("card-title")
+            .text(`${result.title}`);
+          var cardText = $("<p>")
+            .addClass("card-text")
+            .text(`Ready in ${result.readyInMinutes} minutes!`);
+          var stepsButton = $("<a>")
+            .attr("type", "button")
+            .attr("id", "viewRecipeBtn")
+            .addClass("btn btn-primary")
+            .attr("data-toggle", "modal")
+            .attr("data-target", "#recipeModal")
+            .text("View Recipe");
+          $(".card-box").append(
+            cardDiv.append(image),
+            contentDiv.append(cardTitle, cardText, stepsButton)
+          );
+          // console.log(result.analyzedInstructions);
+          // console.log(`Query URL: ${queryUrl}`);
+          $(document).on("click", "#viewRecipeBtn", function(event) {
+            event.preventDefault();
+            console.log(this);
+            $("#recipeModal").modal("show");
+
+            $("#modalTitle").text(`${result.name}`);
+            $("#modalBody").text(`${stepNumber}. ${stepText}`);
+          });
+        }
+      },
+      function(error) {
+        console.log(error);
+      }
+    );
+  }
+
+  //loop through array of ID's passed through
+}
+
+//make a function that creates an object of recipe information and render it through handlebars
